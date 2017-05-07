typetetris/ghc

rts: Emit debug information about program to event log
This commit adds support to the RTS for traversing the debug information
present in a program and emitting it to the event log for later
consumption by debugging and performance analysis tools. This includes
source note, symbol, and address range information derived from the
DWARF annotations produced by GHC.

Unfortunately one function necessary for traversing GHC's extended DWARF
information, `dwarf_cu_getdwarf`, was only introduced in libdw 0.160.
Consequently we won't be able to support statistical profiling in
releases earlier than this.

rts/Trace.c
@@ -621,6 +621,30 @@ void traceTaskDelete_ (Task *task)
     }
 }
 
+void traceProc(const char *name)
+{
+    if (eventlog_enabled) postProcEvent(name);
+}
+
+void traceProcEnd()
+{
+    if (eventlog_enabled) postProcEndEvent();
+}
+
+void traceProcRange(StgWord start, StgWord end)
+{
+    if (eventlog_enabled) postProcRangeEvent(start, end);
+}
+
+void traceProcSourceNote(const char *name,
+                         nat start_line, nat start_col,
+                         nat end_line, nat end_col)
+{
+    if (eventlog_enabled)
+        postProcSourceNoteEvent(name, start_line, start_col,
+                                end_line, end_col);
+}
+
 #ifdef DEBUG
 static void vtraceCap_stderr(Capability *cap, char *msg, va_list ap)
 {

rts/Trace.h
@@ -275,6 +275,23 @@ void traceTaskMigrate_ (Task       *task,
 
 void traceTaskDelete_ (Task       *task);
 
+/*
+ * Events for emitting debug information describing the program to the eventlog
+ *
+ * These are used by LibdwScrape to emit information from the DWARF annotations
+ * in the loaded modules to the event log for later use by debugging and
+ * analysis tools.
+ */
+void traceProc(const char *name);
+
+void traceProcEnd(void);
+
+void traceProcRange(StgWord start, StgWord end);
+
+void traceProcSourceNote(const char *name,
+                         nat start_line, nat start_col,
+                         nat end_line, nat end_col);
+
 #else /* !TRACING */
 
 #define traceSchedEvent(cap, tag, tso, other) /* nothing */
@@ -474,7 +491,7 @@ void dtraceUserMarkerWrapper(Capability *cap, char *msg);
 #define dtraceTaskMigrate(taskID, cap, new_cap)         /* nothing */
 #define dtraceTaskDelete(taskID)                        /* nothing */
 
-#endif
+#endif /* DTRACE */
 
 // -----------------------------------------------------------------------------
 // Trace probes dispatching to various tracing frameworks

rts/eventlog/EventLog.c
@@ -106,6 +106,10 @@ char *EventDesc[] = {
   [EVENT_TASK_MIGRATE]        = "Task migrate",
   [EVENT_TASK_DELETE]         = "Task delete",
   [EVENT_HACK_BUG_T9003]      = "Empty event for bug #9003",
+  [EVENT_PROC]                = "Procedure information",
+  [EVENT_PROC_END]            = "End of previous EVENT_PROC node",
+  [EVENT_PROC_RANGE]          = "Procedure address range information",
+  [EVENT_PROC_SOURCE_NOTE]    = "Procedure source note",
 };
 
 // Event type.
@@ -209,6 +213,7 @@ static inline void postInt8(EventsBuf *eb, StgInt8 i)
 static inline void postInt32(EventsBuf *eb, StgInt32 i)
 { postWord32(eb, (StgWord32)i); }
 
+#define EVENT_SIZE_DYNAMIC (-1)
 
 void
 initEventLogging(void)
@@ -427,6 +432,22 @@ initEventLogging(void)
 
         case EVENT_HACK_BUG_T9003:
             eventTypes[t].size = 0;
+            break;
+
+        case EVENT_PROC:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
+            break;
+
+        case EVENT_PROC_END:
+            eventTypes[t].size = 0;
+            break;
+
+        case EVENT_PROC_RANGE:
+            eventTypes[t].size = 2 * sizeof(StgWord64);
+            break;
+
+        case EVENT_PROC_SOURCE_NOTE:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
             break;
 
         default:
@@ -959,6 +980,63 @@ void postTaskDeleteEvent (EventTaskId taskId)
     postEventHeader(&eventBuf, EVENT_TASK_DELETE);
     /* EVENT_TASK_DELETE (taskID) */
     postTaskId(&eventBuf, taskId);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcEvent(const char *name)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    nat size = strlen(name);
+    ensureRoomForVariableEvent(&eventBuf, size);
+
+    postEventHeader(&eventBuf, EVENT_PROC);
+    postPayloadSize(&eventBuf, size);
+    /* EVENT_PROC (name) */
+    postBuf(&eventBuf, (StgWord8*) name, size);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcEndEvent()
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    ensureRoomForEvent(&eventBuf, EVENT_PROC_END);
+    postEventHeader(&eventBuf, EVENT_PROC_END);
+    /* EVENT_PROC_END */
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcRangeEvent(StgWord start, StgWord end)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    ensureRoomForEvent(&eventBuf, EVENT_PROC_RANGE);
+
+    postEventHeader(&eventBuf, EVENT_PROC_RANGE);
+    /* EVENT_PROC_RANGE (start, end) */
+    postWord64(&eventBuf, start);
+    postWord64(&eventBuf, end);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcSourceNoteEvent(const char *name,
+                             nat start_line, nat start_col,
+                             nat end_line, nat end_col)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    nat name_sz = strlen(name) + 1; // Include \0 byte
+    nat size = name_sz + 4*4;
+    ensureRoomForVariableEvent(&eventBuf, size);
+
+    postEventHeader(&eventBuf, EVENT_PROC_SOURCE_NOTE);
+    postPayloadSize(&eventBuf, size);
+    /* EVENT_PROC_SOURCE_NOTE (name, start_line, start_col, end_line, end_col)*/
+    postBuf(&eventBuf, (StgWord8*) name, name_sz);
+    postWord32(&eventBuf, start_line);
+    postWord32(&eventBuf, start_col);
+    postWord32(&eventBuf, end_line);
+    postWord32(&eventBuf, end_col);
 
     RELEASE_LOCK(&eventBufMutex);
 }

rts/eventlog/EventLog.h
@@ -133,6 +133,24 @@ void postTaskMigrateEvent (EventTaskId taskId,
 
 void postTaskDeleteEvent (EventTaskId taskId);
 
+/*
+ * Events for emitting debug information describing the program to the eventlog
+ *
+ * These are used by LibdwScrape to emit information from the DWARF annotations
+ * in the loaded modules to the event log for later use by debugging and
+ * analysis tools.
+ */
+
+void postProcEvent(const char *name);
+
+void postProcEndEvent(void);
+
+void postProcRangeEvent(StgWord start, StgWord end);
+
+void postProcSourceNoteEvent(const char *name,
+                             nat start_line, nat start_col,
+                             nat end_line, nat end_col);
+
 #else /* !TRACING */
 
 INLINE_HEADER void postSchedEvent (Capability *cap  STG_UNUSED,

CIFASIS/ghc-cm

rts/Trace.c
@@ -648,6 +648,30 @@ void traceHeapProfSampleCostCentre(StgWord8 profile_id,
 }
 #endif
 
+void traceProc(const char *name)
+{
+    if (eventlog_enabled) postProcEvent(name);
+}
+
+void traceProcEnd()
+{
+    if (eventlog_enabled) postProcEndEvent();
+}
+
+void traceProcRange(StgWord start, StgWord end)
+{
+    if (eventlog_enabled) postProcRangeEvent(start, end);
+}
+
+void traceProcSourceNote(const char *name,
+                         uint32_t start_line, uint32_t start_col,
+                         uint32_t end_line, uint32_t end_col)
+{
+    if (eventlog_enabled)
+        postProcSourceNoteEvent(name, start_line, start_col,
+                                end_line, end_col);
+}
+
 #if defined(DEBUG)
 static void vtraceCap_stderr(Capability *cap, char *msg, va_list ap)
 {

rts/Trace.h
@@ -294,6 +294,23 @@ void traceHeapProfSampleCostCentre(StgWord8 profile_id,
                                    CostCentreStack *stack, StgWord residency);
 #endif /* PROFILING */
 
+/*
+ * Events for emitting debug information describing the program to the eventlog
+ *
+ * These are used by LibdwScrape to emit information from the DWARF annotations
+ * in the loaded modules to the event log for later use by debugging and
+ * analysis tools.
+ */
+void traceProc(const char *name);
+
+void traceProcEnd(void);
+
+void traceProcRange(StgWord start, StgWord end);
+
+void traceProcSourceNote(const char *name,
+                         uint32_t start_line, uint32_t start_col,
+                         uint32_t end_line, uint32_t end_col);
+
 #else /* !TRACING */
 
 #define traceSchedEvent(cap, tag, tso, other) /* nothing */
@@ -498,7 +515,7 @@ void dtraceUserMarkerWrapper(Capability *cap, char *msg);
 #define dtraceTaskMigrate(taskID, cap, new_cap)         /* nothing */
 #define dtraceTaskDelete(taskID)                        /* nothing */
 
-#endif
+#endif /* DTRACE */
 
 // -----------------------------------------------------------------------------
 // Trace probes dispatching to various tracing frameworks

rts/eventlog/EventLog.c
@@ -100,6 +100,10 @@ char *EventDesc[] = {
   [EVENT_TASK_MIGRATE]        = "Task migrate",
   [EVENT_TASK_DELETE]         = "Task delete",
   [EVENT_HACK_BUG_T9003]      = "Empty event for bug #9003",
+  [EVENT_PROC]                = "Procedure information",
+  [EVENT_PROC_END]            = "End of previous EVENT_PROC node",
+  [EVENT_PROC_RANGE]          = "Procedure address range information",
+  [EVENT_PROC_SOURCE_NOTE]    = "Procedure source note",
   [EVENT_HEAP_PROF_BEGIN]     = "Start of heap profile",
   [EVENT_HEAP_PROF_COST_CENTRE]   = "Cost center definition",
   [EVENT_HEAP_PROF_SAMPLE_BEGIN]  = "Start of heap profile sample",
@@ -443,6 +447,22 @@ initEventLogging(const EventLogWriter *ev_writer)
 
         case EVENT_HACK_BUG_T9003:
             eventTypes[t].size = 0;
+            break;
+
+        case EVENT_PROC:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
+            break;
+
+        case EVENT_PROC_END:
+            eventTypes[t].size = 0;
+            break;
+
+        case EVENT_PROC_RANGE:
+            eventTypes[t].size = 2 * sizeof(StgWord64);
+            break;
+
+        case EVENT_PROC_SOURCE_NOTE:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
             break;
 
         case EVENT_HEAP_PROF_BEGIN:
@@ -980,6 +1000,63 @@ void postTaskDeleteEvent (EventTaskId taskId)
     postEventHeader(&eventBuf, EVENT_TASK_DELETE);
     /* EVENT_TASK_DELETE (taskID) */
     postTaskId(&eventBuf, taskId);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcEvent(const char *name)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    uint32_t size = strlen(name);
+    ensureRoomForVariableEvent(&eventBuf, size);
+
+    postEventHeader(&eventBuf, EVENT_PROC);
+    postPayloadSize(&eventBuf, size);
+    /* EVENT_PROC (name) */
+    postBuf(&eventBuf, (StgWord8*) name, size);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcEndEvent()
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    ensureRoomForEvent(&eventBuf, EVENT_PROC_END);
+    postEventHeader(&eventBuf, EVENT_PROC_END);
+    /* EVENT_PROC_END */
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcRangeEvent(StgWord start, StgWord end)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    ensureRoomForEvent(&eventBuf, EVENT_PROC_RANGE);
+
+    postEventHeader(&eventBuf, EVENT_PROC_RANGE);
+    /* EVENT_PROC_RANGE (start, end) */
+    postWord64(&eventBuf, start);
+    postWord64(&eventBuf, end);
+
+    RELEASE_LOCK(&eventBufMutex);
+}
+
+void postProcSourceNoteEvent(const char *name,
+                             uint32_t start_line, uint32_t start_col,
+                             uint32_t end_line, uint32_t end_col)
+{
+    ACQUIRE_LOCK(&eventBufMutex);
+    uint32_t name_sz = strlen(name) + 1; // Include \0 byte
+    uint32_t size = name_sz + 4*4;
+    ensureRoomForVariableEvent(&eventBuf, size);
+
+    postEventHeader(&eventBuf, EVENT_PROC_SOURCE_NOTE);
+    postPayloadSize(&eventBuf, size);
+    /* EVENT_PROC_SOURCE_NOTE (name, start_line, start_col, end_line, end_col)*/
+    postBuf(&eventBuf, (StgWord8*) name, name_sz);
+    postWord32(&eventBuf, start_line);
+    postWord32(&eventBuf, start_col);
+    postWord32(&eventBuf, end_line);
+    postWord32(&eventBuf, end_col);
 
     RELEASE_LOCK(&eventBufMutex);
 }

rts/eventlog/EventLog.h
@@ -133,6 +133,24 @@ void postTaskMigrateEvent (EventTaskId taskId,
 
 void postTaskDeleteEvent (EventTaskId taskId);
 
+/*
+ * Events for emitting debug information describing the program to the eventlog
+ *
+ * These are used by LibdwScrape to emit information from the DWARF annotations
+ * in the loaded modules to the event log for later use by debugging and
+ * analysis tools.
+ */
+
+void postProcEvent(const char *name);
+
+void postProcEndEvent(void);
+
+void postProcRangeEvent(StgWord start, StgWord end);
+
+void postProcSourceNoteEvent(const char *name,
+                             uint32_t start_line, uint32_t start_col,
+                             uint32_t end_line, uint32_t end_col);
+
 void postHeapProfBegin(StgWord8 profile_id);
 
 void postHeapProfSampleBegin(StgInt era);