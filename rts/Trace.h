typetetris/ghc

Add infrastructure for a simple statistical profiler

rts/Trace.h
@@ -292,6 +292,18 @@ void traceProcSourceNote(const char *name,
                          nat start_line, nat start_col,
                          nat end_line, nat end_col);
 
+/* Statistical profiling */
+#ifdef STAT_PROFILE
+/* SAMPLE_BY_*: Various things we can trigger samples on */
+
+/* SAMPLE_TYPE_*: Various things we can collect in our samples */
+#define SAMPLE_TYPE_INSTR_PTR     0
+
+void traceStatProfileSamples(Capability *cap, StgBool own_cap,
+                             StgWord32 sample_by, StgWord32 sample_type,
+                             StgWord32 cnt, void **samples, nat *weights);
+#endif /* STAT_PROFILE */
+
 #else /* !TRACING */
 
 #define traceSchedEvent(cap, tag, tso, other) /* nothing */

rts/eventlog/EventLog.c
@@ -110,6 +110,7 @@ char *EventDesc[] = {
   [EVENT_PROC_END]            = "End of previous EVENT_PROC node",
   [EVENT_PROC_RANGE]          = "Procedure address range information",
   [EVENT_PROC_SOURCE_NOTE]    = "Procedure source note",
+  [EVENT_STAT_PROF_SAMPLES]   = "Statistical profiler samples",
 };
 
 // Event type.
@@ -160,6 +161,19 @@ static inline void postWord64(EventsBuf *eb, StgWord64 i)
 {
     postWord32(eb, (StgWord32)(i >> 32));
     postWord32(eb, (StgWord32)i);
+}
+
+static inline void postWord8at(EventsBuf *eb STG_UNUSED,
+                               StgWord8 i, StgInt8 *pos)
+{
+    ASSERT(pos >= eb->begin && pos < eb->pos);
+    *pos = i;
+}
+
+static inline void postWord16at(EventsBuf *eb, StgWord16 i, StgInt8 *pos)
+{
+    postWord8at(eb, (StgWord8)(i >> 8), pos);
+    postWord8at(eb, (StgWord8)i,        pos+1);
 }
 
 static inline void postBuf(EventsBuf *eb, StgWord8 *buf, nat size)
@@ -447,6 +461,10 @@ initEventLogging(void)
             break;
 
         case EVENT_PROC_SOURCE_NOTE:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
+            break;
+
+        case EVENT_STAT_PROF_SAMPLES:
             eventTypes[t].size = EVENT_SIZE_DYNAMIC;
             break;
 
@@ -1040,6 +1058,106 @@ void postProcSourceNoteEvent(const char *name,
 
     RELEASE_LOCK(&eventBufMutex);
 }
+
+#ifdef STAT_PROFILE
+void postStatProfileSamples(
+    Capability *cap, StgBool own_cap,
+    StgWord32 sample_by, StgWord32 sample_type,
+    StgWord32 cnt, void **samples, nat *weights)
+{
+    // (size:16, cap:16, verb:8, noun:8, various )
+    nat hdr_size = sizeof(EventCapNo) + 2 * sizeof(StgWord8);
+    nat est_size = hdr_size + cnt * sizeof(StgWord64);
+    EventsBuf *eb = own_cap ? &capEventBuf[cap->no] : &eventBuf;
+    if (ensureRoomForVariableEvent(eb, est_size)) {
+        return;
+    }
+    postEventHeader(eb, EVENT_STAT_PROF_SAMPLES);
+    StgInt8 *size_pos = eb->pos;
+    postPayloadSize(eb, 0);
+    postCapNo(eb, cap->no);
+    postWord8(eb, sample_by);
+    postWord8(eb, sample_type);
+
+    // We actually put quite a bit of effort into compressing the
+    // samples here. The basic idea is that we will often have samples
+    // in very close proximity, which can be exploited. Note that we
+    // *might* end up using more space than estimated, in which case
+    // this event might get split up.The encoding is:
+    //  (sample_encoding:4, weight_encoding:4, sample, weight)
+    // with sample encoding types being:
+    //  0  = 8-bit offset to previous address
+    //  1  = reverse 8-bit offset to previous address
+    //  4  = 32-bit offset to previous address
+    //  5  = reverse 32-bit offset to previous address
+    //  15 = direct encoding
+    // and weight encoding being simply the number of bytes used to
+    // encode the weight. If zero bytes are used for a weight, this
+    // implies weight 1.
+    nat i = 0, done = 0, weight = 0;
+    StgWord64 last = 0;
+    for (; i < cnt; i++) {
+        weight += (weights ? weights[i] : 1);
+        // Next entry the same? compress
+        if (i+1 < cnt && samples[i] == samples[i+1])
+            continue;
+        // Weight encoding
+        nat wbytes;
+        if (weight == 1)               wbytes = 0;
+        else if (weight <= 0xff)       wbytes = 1;
+        else if (weight <= 0xffff)     wbytes = 2;
+        else if (weight <= 0xffffffff) wbytes = 4;
+        else                           wbytes = 8;
+        // Similar to last entry?
+        StgWord cur = (StgWord) samples[i];
+        #define CHECK_WRITE(n) \
+            if(eb->pos + (n) >= eb->begin + eb->size) break;
+        if (cur - last <= 0xff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x00 | wbytes);
+            postWord8(eb, (StgWord8) (cur - last));
+        } else if (last - cur <= 0xff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x10 | wbytes);
+            postWord8(eb, (StgWord8) (last - cur));
+        } else if (cur - last <= 0xffffffff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x40 | wbytes);
+            postWord32(eb, (StgWord32) (cur - last));
+        } else if (last - cur <= 0xffffffff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x50 | wbytes);
+            postWord32(eb, (StgWord32) (last - cur));
+        } else {
+            CHECK_WRITE(9 + wbytes);
+            postWord8(eb, 0xf0 | wbytes);
+            postWord64(eb, (StgWord64) cur);
+        }
+        #undef CHECK_WRITE
+        switch(wbytes) {
+        case 0: break;
+        case 1: postWord8(eb, (StgWord8) weight); break;
+        case 2: postWord16(eb, (StgWord16) weight); break;
+        case 4: postWord32(eb, (StgWord32) weight); break;
+        case 8: postWord64(eb, (StgWord64) weight); break;
+        }
+        // Prepare writing next entry
+        last = cur;
+        weight = 0;
+        done = i+1;
+    }
+    // Determine and write final length
+    EventPayloadSize size = eb->pos - size_pos - sizeof(EventPayloadSize);
+    postWord16at(eb, size, size_pos);
+    // Samples left for output? Generate another message
+    if (done < cnt) {
+        printAndClearEventBuf(eb);
+        postStatProfileSamples(cap, own_cap,
+                               sample_by, sample_type,
+                               cnt-done, samples+done, weights+done);
+    }
+}
+#endif /* STAT_PROFILE */
 
 void
 postEvent (Capability *cap, EventTypeNum tag)

rts/eventlog/EventLog.h
@@ -151,6 +151,13 @@ void postProcSourceNoteEvent(const char *name,
                              nat start_line, nat start_col,
                              nat end_line, nat end_col);
 
+/* Statistical profiler */
+#ifdef STAT_PROFILE
+void postStatProfileSamples(Capability *cap, StgBool own_cap,
+                            StgWord32 sample_by, StgWord32 sample_type,
+                            StgWord32 cnt, void **samples, nat *weights);
+#endif /* STAT_PROFILE */
+
 #else /* !TRACING */
 
 INLINE_HEADER void postSchedEvent (Capability *cap  STG_UNUSED,

CIFASIS/ghc-cm

rts/Trace.h
@@ -311,6 +311,18 @@ void traceProcSourceNote(const char *name,
                          uint32_t start_line, uint32_t start_col,
                          uint32_t end_line, uint32_t end_col);
 
+/* Statistical profiling */
+#if defined(STAT_PROFILE)
+/* SAMPLE_BY_*: Various things we can trigger samples on */
+
+/* SAMPLE_TYPE_*: Various things we can collect in our samples */
+#define SAMPLE_TYPE_INSTR_PTR     0
+
+void traceStatProfileSamples(Capability *cap, bool own_cap,
+                             uint32_t sample_by, uint32_t sample_type,
+                             uint32_t cnt, void **samples, uint32_t *weights);
+#endif /* STAT_PROFILE */
+
 #else /* !TRACING */
 
 #define traceSchedEvent(cap, tag, tso, other) /* nothing */

rts/eventlog/EventLog.c
@@ -109,6 +109,7 @@ char *EventDesc[] = {
   [EVENT_HEAP_PROF_SAMPLE_BEGIN]  = "Start of heap profile sample",
   [EVENT_HEAP_PROF_SAMPLE_STRING] = "Heap profile string sample",
   [EVENT_HEAP_PROF_SAMPLE_COST_CENTRE] = "Heap profile cost-centre sample",
+  [EVENT_STAT_PROF_SAMPLES]   = "Statistical profiler samples",
 };
 
 // Event type.
@@ -159,6 +160,19 @@ static inline void postWord64(EventsBuf *eb, StgWord64 i)
 {
     postWord32(eb, (StgWord32)(i >> 32));
     postWord32(eb, (StgWord32)i);
+}
+
+static inline void postWord8at(EventsBuf *eb STG_UNUSED,
+                               StgWord8 i, StgInt8 *pos)
+{
+    ASSERT(pos >= eb->begin && pos < eb->pos);
+    *pos = i;
+}
+
+static inline void postWord16at(EventsBuf *eb, StgWord16 i, StgInt8 *pos)
+{
+    postWord8at(eb, (StgWord8)(i >> 8), pos);
+    postWord8at(eb, (StgWord8)i,        pos+1);
 }
 
 static inline void postBuf(EventsBuf *eb, StgWord8 *buf, uint32_t size)
@@ -462,6 +476,10 @@ initEventLogging(const EventLogWriter *ev_writer)
             break;
 
         case EVENT_PROC_SOURCE_NOTE:
+            eventTypes[t].size = EVENT_SIZE_DYNAMIC;
+            break;
+
+        case EVENT_STAT_PROF_SAMPLES:
             eventTypes[t].size = EVENT_SIZE_DYNAMIC;
             break;
 
@@ -1060,6 +1078,106 @@ void postProcSourceNoteEvent(const char *name,
 
     RELEASE_LOCK(&eventBufMutex);
 }
+
+#if defined(STAT_PROFILE)
+void postStatProfileSamples(
+    Capability *cap, bool own_cap,
+    uint32_t sample_by, uint32_t sample_type,
+    uint32_t cnt, void **samples, uint32_t *weights)
+{
+    // (size:16, cap:16, verb:8, noun:8, various )
+    uint32_t hdr_size = sizeof(EventCapNo) + 2 * sizeof(StgWord8);
+    uint32_t est_size = hdr_size + cnt * sizeof(StgWord64);
+    EventsBuf *eb = own_cap ? &capEventBuf[cap->no] : &eventBuf;
+    if (ensureRoomForVariableEvent(eb, est_size)) {
+        return;
+    }
+    postEventHeader(eb, EVENT_STAT_PROF_SAMPLES);
+    StgInt8 *size_pos = eb->pos;
+    postPayloadSize(eb, 0);
+    postCapNo(eb, cap->no);
+    postWord8(eb, sample_by);
+    postWord8(eb, sample_type);
+
+    // We actually put quite a bit of effort into compressing the
+    // samples here. The basic idea is that we will often have samples
+    // in very close proximity, which can be exploited. Note that we
+    // *might* end up using more space than estimated, in which case
+    // this event might get split up.The encoding is:
+    //  (sample_encoding:4, weight_encoding:4, sample, weight)
+    // with sample encoding types being:
+    //  0  = 8-bit offset to previous address
+    //  1  = reverse 8-bit offset to previous address
+    //  4  = 32-bit offset to previous address
+    //  5  = reverse 32-bit offset to previous address
+    //  15 = direct encoding
+    // and weight encoding being simply the number of bytes used to
+    // encode the weight. If zero bytes are used for a weight, this
+    // implies weight 1.
+    uint32_t i = 0, done = 0, weight = 0;
+    StgWord64 last = 0;
+    for (; i < cnt; i++) {
+        weight += (weights ? weights[i] : 1);
+        // Next entry the same? compress
+        if (i+1 < cnt && samples[i] == samples[i+1])
+            continue;
+        // Weight encoding
+        uint32_t wbytes;
+        if (weight == 1)               wbytes = 0;
+        else if (weight <= 0xff)       wbytes = 1;
+        else if (weight <= 0xffff)     wbytes = 2;
+        else if (weight <= 0xffffffff) wbytes = 4;
+        else                           wbytes = 8;
+        // Similar to last entry?
+        StgWord cur = (StgWord) samples[i];
+        #define CHECK_WRITE(n) \
+            if(eb->pos + (n) >= eb->begin + eb->size) break;
+        if (cur - last <= 0xff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x00 | wbytes);
+            postWord8(eb, (StgWord8) (cur - last));
+        } else if (last - cur <= 0xff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x10 | wbytes);
+            postWord8(eb, (StgWord8) (last - cur));
+        } else if (cur - last <= 0xffffffff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x40 | wbytes);
+            postWord32(eb, (StgWord32) (cur - last));
+        } else if (last - cur <= 0xffffffff) {
+            CHECK_WRITE(2 + wbytes);
+            postWord8(eb, 0x50 | wbytes);
+            postWord32(eb, (StgWord32) (last - cur));
+        } else {
+            CHECK_WRITE(9 + wbytes);
+            postWord8(eb, 0xf0 | wbytes);
+            postWord64(eb, (StgWord64) cur);
+        }
+        #undef CHECK_WRITE
+        switch(wbytes) {
+        case 0: break;
+        case 1: postWord8(eb, (StgWord8) weight); break;
+        case 2: postWord16(eb, (StgWord16) weight); break;
+        case 4: postWord32(eb, (StgWord32) weight); break;
+        case 8: postWord64(eb, (StgWord64) weight); break;
+        }
+        // Prepare writing next entry
+        last = cur;
+        weight = 0;
+        done = i+1;
+    }
+    // Determine and write final length
+    EventPayloadSize size = eb->pos - size_pos - sizeof(EventPayloadSize);
+    postWord16at(eb, size, size_pos);
+    // Samples left for output? Generate another message
+    if (done < cnt) {
+        printAndClearEventBuf(eb);
+        postStatProfileSamples(cap, own_cap,
+                               sample_by, sample_type,
+                               cnt-done, samples+done, weights+done);
+    }
+}
+#endif /* STAT_PROFILE */
 
 void
 postEvent (Capability *cap, EventTypeNum tag)

rts/eventlog/EventLog.h
@@ -151,6 +151,13 @@ void postProcSourceNoteEvent(const char *name,
                              uint32_t start_line, uint32_t start_col,
                              uint32_t end_line, uint32_t end_col);
 
+/* Statistical profiler */
+#if defined(STAT_PROFILE)
+void postStatProfileSamples(Capability *cap, bool own_cap,
+                            uint32_t sample_by, uint32_t sample_type,
+                            uint32_t cnt, void **samples, uint32_t *weights);
+#endif /* STAT_PROFILE */
+
 void postHeapProfBegin(StgWord8 profile_id);
 
 void postHeapProfSampleBegin(StgInt era);