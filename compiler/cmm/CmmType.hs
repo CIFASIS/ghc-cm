1fd74fcc798e4da07fdefb8d02dff479abe27d34
Gabor Greif <ggreif@gmail.com>
2017-09-05 17:13:46 +0200

    Simplify mrStr

--- compiler/cmm/CmmType.hs
+++ compiler/cmm/CmmType.hs
@@ -177,14 +177,7 @@ instance Outputable Width where
    ppr rep = ptext (mrStr rep)
 
 mrStr :: Width -> LitString
-mrStr W8   = sLit("W8")
-mrStr W16  = sLit("W16")
-mrStr W32  = sLit("W32")
-mrStr W64  = sLit("W64")
-mrStr W128 = sLit("W128")
-mrStr W256 = sLit("W256")
-mrStr W512 = sLit("W512")
-mrStr W80  = sLit("W80")
+mrStr = sLit . show
 
 
 -------- Common Widths  ------------
